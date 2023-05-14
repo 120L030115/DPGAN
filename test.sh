b64153a4246fa671c458ca17197b36795af1fbab
呆先森HIT <93043443+120L030115@users.noreply.github.com>
2023-05-14 21:01:19 +0800

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,2 @@
+python test.py --name dp_gan_flickr --dataset_mode flickr --gpu_ids 0 --dataroot datasets/flickr --batch_size 1
+
---
+++ train.sh
@@ -0,0 +1,1 @@
+python train.py --name dp_gan_flickr --dataset_mode flickr --gpu_ids 0 --dataroot datasets/flickr --batch_size 1
